6eb3fc1960d4dfcb4129a34519193d8d794efc5b
Dai Niu <niudai>
2023-02-01 01:01:13 +0800

    fix

--- comps.js
+++ comps.js
@@ -2,14 +2,45 @@ import { Visual } from "@raber/react";
 import { Button, ButtonGroup } from '@chakra-ui/react'
 
 export const VButton = Visual.Comp(({
-    variant
+    variant,
+    text
 }) => {
-    const vVariant = useEnum('Variant', ['solid', 'outline', 'ghost', 'unstyled', 'link'], {
-        default: variant
+
+    // 每次组件渲染额时候，上报一次 UI 配置
+    // 每次组件渲染时，从 context 读取 props 配置，然后渲染组件，
+    // 不从 props 注入，采用截获的方式
+
+    // 构思文案，子组件：
+
+    // 子组件必须有一个唯一的 key，用于区分不同的子组件
+
+    const styles = useStyle();
+
+
+    const vVariant = useEnum('variant', 
+    {
+        key: 'variant',
+        default: variant,
+        uiConfig: {
+            type: 'select',
+            options: [
+                { label: 'Solid', value: 'solid' },
+                { label: 'Outline', value: 'outline' },
+                { label: 'Ghost', value: 'ghost' },
+                { label: 'Unstyled', value: 'unstyled' },
+                { label: 'Link', value: 'link' },
+            ]
+        },
+        title: '按钮类型'
     });
 
     const vText = useText('Text', {
+        key: 'text',
         default: text,
+        uiConfig: {
+            type: 'input',
+            placeholder: '请输入文本'
+        },
         title: '内部文本'
         });
 
