4b8b16f00cb5891a87888e3c941ad0f8aa882f65
niudai <niudai.geek@bytedance.com>
2023-03-12 19:47:54 +0800

    xxxs

--- api/revalidate.mjs
+++ api/revalidate.mjs
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
       await res.revalidate(req.query.path)
       return res.json({ revalidated: true })
     } catch (err) {
+      console.error(err);
       // If there was an error, Next.js will continue
       // to show the last successfully generated page
       return res.status(500).send('Error revalidating')
